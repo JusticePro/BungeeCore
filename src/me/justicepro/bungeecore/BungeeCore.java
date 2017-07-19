4f3c3f999bf82244a633a390928a827f9dd42768
JusticePro <justicedbenezra@gmail.com>
2017-07-19 17:57:29 -0500

    Updated

--- src/me/justicepro/bungeecore/BungeeCore.java
+++ src/me/justicepro/bungeecore/BungeeCore.java
@@ -1,8 +1,9 @@
 package me.justicepro.bungeecore;
 
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Plugin;
 
-public class Plugin extends net.md_5.bungee.api.plugin.Plugin {
+public class BungeeCore extends Plugin {
 	
 	@Override
 	public void onEnable() {
