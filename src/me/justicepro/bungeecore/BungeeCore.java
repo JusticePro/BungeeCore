de7bd8980977006360b632280945d2bda4139232
JusticePro <justicedbenezra@gmail.com>
2017-07-19 18:46:21 -0500

    Updated and fixed Bungeecore

--- src/me/justicepro/bungeecore/BungeeCore.java
+++ src/me/justicepro/bungeecore/BungeeCore.java
@@ -1,13 +1,12 @@
 package me.justicepro.bungeecore;
 
-import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class BungeeCore extends Plugin {
 	
 	@Override
 	public void onEnable() {
-		ProxyServer.getInstance().getPluginManager().registerCommand(this, new ServerCommand());
+		getProxy().getPluginManager().registerCommand(this, new ServerCommand());
 	}
 	
 }
--- src/me/justicepro/bungeecore/ServerCommand.java
+++ src/me/justicepro/bungeecore/ServerCommand.java
@@ -3,7 +3,7 @@ package me.justicepro.bungeecore;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
@@ -21,13 +21,18 @@ public class ServerCommand extends Command {
 			ProxiedPlayer player = (ProxiedPlayer) sender;
 
 			if (args.length>=1) {
-				ServerInfo target = ProxyServer.getInstance().getServerInfo(args[0]);
-				player.sendMessage(new ComponentBuilder(ChatColor.translateAlternateColorCodes('&',
-						"&9Server>&7 You have been from &6" + player.getServer().getInfo().getName() + "&7 to &6" + args[0])).create());
-				player.connect(target);
+				try {
+					ServerInfo target = ProxyServer.getInstance().getServerInfo(args[0]);
+					player.connect(target);
+					player.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&',
+							"&9Portal>&7 You have been from &6" + player.getServer().getInfo().getName() + "&7 to &6" + args[0] + "&7.")));
+				}catch (Exception e) {
+					player.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&',
+							"&9Portal>&7 Couldn't find server &6" + args[0] + "&7.")));
+				}
 			}else {
-				player.sendMessage(new ComponentBuilder(ChatColor.translateAlternateColorCodes('&',
-						"&9Portal>&7 You are on server &6" + player.getServer().getInfo().getName())).create());
+				player.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&',
+						"&9Portal>&7 You are on server &6" + player.getServer().getInfo().getName() + "&7.")));
 			}
 
 		}
